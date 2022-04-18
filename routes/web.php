ec49fdb23d121c066919f60c3398fb6febc4ba3c
ChrysostomosKas <57672898+ChrysostomosKas@users.noreply.github.com>
2022-04-18 17:07:10 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+
+    return view('posts', [
+        'posts' => Post::all()
+        ]);
+});
+
+
+
+// Find a post by its slug and pass it to a view called "post "
+Route::get('posts/{post}', function ($slug) {
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+
+})->where('post', '[A-z_\-]+');
