91fad0cdc17ba68f4f0952769eff74505c560d3c
ChrysostomosKas <57672898+ChrysostomosKas@users.noreply.github.com>
2022-04-20 17:01:42 +0300

    Episode 28 -> Turbo Boost With Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,54 +25,13 @@ class DatabaseSeeder extends Seeder
          * but not the table itself.
          * Then run the rest code
          */
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
 
-
-       $user =  User::factory()->create();
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        $user = User::factory()->create([
+            'name' => 'John Dee'
         ]);
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' =>$family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque orci id placerat rutrum. Quisque ut magna aliquet, molestie ipsum dignissim, interdum purus. Nulla malesuada dui a condimentum facilisis. Ut bibendum magna vitae elementum efficitur. Pellentesque eu varius erat, sed commodo risus. Donec efficitur ornare convallis. Pellentesque hendrerit risus vitae suscipit malesuada. Sed fringilla id enim at euismod. Ut accumsan egestas diam, a aliquam ligula maximus a. Donec non rutrum neque. Duis cursus non tortor id aliquam. Nulla a orci erat. Cras id tristique neque. Etiam volutpat ipsum sit amet nulla pulvinar, ac sagittis felis iaculis. Quisque sagittis, turpis ut consequat gravida, dui purus porttitor dolor, at efficitur velit velit vel ipsum. Vivamus congue felis ultrices mattis cursus</p>'
-            ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' =>$personal->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque orci id placerat rutrum. Quisque ut magna aliquet, molestie ipsum dignissim, interdum purus. Nulla malesuada dui a condimentum facilisis. Ut bibendum magna vitae elementum efficitur. Pellentesque eu varius erat, sed commodo risus. Donec efficitur ornare convallis. Pellentesque hendrerit risus vitae suscipit malesuada. Sed fringilla id enim at euismod. Ut accumsan egestas diam, a aliquam ligula maximus a. Donec non rutrum neque. Duis cursus non tortor id aliquam. Nulla a orci erat. Cras id tristique neque. Etiam volutpat ipsum sit amet nulla pulvinar, ac sagittis felis iaculis. Quisque sagittis, turpis ut consequat gravida, dui purus porttitor dolor, at efficitur velit velit vel ipsum. Vivamus congue felis ultrices mattis cursus</p>'
-            ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' =>$work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque orci id placerat rutrum. Quisque ut magna aliquet, molestie ipsum dignissim, interdum purus. Nulla malesuada dui a condimentum facilisis. Ut bibendum magna vitae elementum efficitur. Pellentesque eu varius erat, sed commodo risus. Donec efficitur ornare convallis. Pellentesque hendrerit risus vitae suscipit malesuada. Sed fringilla id enim at euismod. Ut accumsan egestas diam, a aliquam ligula maximus a. Donec non rutrum neque. Duis cursus non tortor id aliquam. Nulla a orci erat. Cras id tristique neque. Etiam volutpat ipsum sit amet nulla pulvinar, ac sagittis felis iaculis. Quisque sagittis, turpis ut consequat gravida, dui purus porttitor dolor, at efficitur velit velit vel ipsum. Vivamus congue felis ultrices mattis cursus</p>'
-            ]);
 
     }
 
