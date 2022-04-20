b126a3220086a94f92e001c41a69895f93baac39
ChrysostomosKas <57672898+ChrysostomosKas@users.noreply.github.com>
2022-04-20 14:36:38 +0300

    Episode 26 -> Clockwork

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
