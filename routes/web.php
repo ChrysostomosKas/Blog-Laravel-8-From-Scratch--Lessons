731225b73cb05362c7e0cbbb910abe7a28428239
ChrysostomosKas <57672898+ChrysostomosKas@users.noreply.github.com>
2022-04-20 13:40:23 +0300

    Episode 25 -> Show All Posts Associated With a Category

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,10 @@ Route::get('posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
     ]);
+});
 
+Route::get('categories/{category:slug}', function (\App\Models\Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
 });
