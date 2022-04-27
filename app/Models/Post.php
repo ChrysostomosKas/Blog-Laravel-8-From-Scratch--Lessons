329f6783418b16b33b9c9b3c7efd3087138b47f0
ChrysostomosKas <57672898+ChrysostomosKas@users.noreply.github.com>
2022-04-27 15:47:43 +0300

    Episode 43-> Fix a Confusing Eloquent Query Bug

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,11 +18,12 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters) // Post:newQueru()->filter()
     {
-        $query->when($filters['search'] ?? false, function ($query, $search){
-            $query
-                ->whereColumn('title', 'like', '%' . request('search') . '%')
-                ->orWhere('body', 'like', '%' . request('search') . '%');
-        });
+        $query->when($filters['search'] ?? false, fn($query, $search)=>
+            $query->where(fn($query)=>
+                $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('body', 'like', '%' . request('search') . '%')
+        )
+    );
 
         $query->when($filters['category'] ?? false, fn($query, $category) =>
         $query->whereHas('category', fn ($query) =>
