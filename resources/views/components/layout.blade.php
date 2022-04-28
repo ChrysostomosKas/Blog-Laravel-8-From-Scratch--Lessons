32d976db2aa5911076c08a266279a4c9a65d408a
ChrysostomosKas <57672898+ChrysostomosKas@users.noreply.github.com>
2022-04-28 14:30:26 +0300

    Episode 49-> Login and Logout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,8 +14,22 @@
             </a>
         </div>
 
-        <div class="mt-8 md:mt-0">
-            <a href="/" class="text-xs font-bold uppercase">Home Page</a>
+        <div class="mt-8 md:mt-0 flex items-center">
+            {{--If you are in show this msg else show the link--}}
+            @auth()
+                <span class="text-xs font-bold uppercase">Welcome, {{auth()->user()->name}}!</span>
+
+                <form method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6">
+                    @csrf
+
+                    <button type="submit">
+                        Log Out
+                    </button>
+                </form>
+            @else
+                <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
+            @endauth
 
             <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                 Subscribe for Updates
@@ -53,7 +67,7 @@
         </div>
     </footer>
 </section>
-    {{--Call component for create acc msg for 2sec--}}
-    <x-flash>
-    </x-flash>
+{{--Call component for create acc msg for 2sec--}}
+<x-flash>
+</x-flash>
 </body>
