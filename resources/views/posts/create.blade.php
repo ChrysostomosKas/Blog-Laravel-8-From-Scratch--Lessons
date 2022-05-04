68132bc8d94fcec5f6c6ac6d78e56827b71fef48
ChrysostomosKas <57672898+ChrysostomosKas@users.noreply.github.com>
2022-05-04 12:32:04 +0300

    Episode 64->Validate and Store Post Thumbnails

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,8 +1,11 @@
 <x-layout>
-    <section class="px-6 py-8">
-        <x-panel class="max-w-sm mx-auto">
+    <section class="py-8 max-w-md mx-auto">
+        <h1 class="text-lg font-bold mb-4">
+            Publish New Post
+        </h1>
+        <x-panel>
 
-            <form method="POST" action="/admin/posts">
+            <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-sx text-gray-700"
@@ -44,6 +47,25 @@
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-sx text-gray-700"
+                           for="thumbnail"
+                    >
+                        Thumbnail
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="file"
+                           name="thumbnail"
+                           id="thumbnail"
+                           value="{{old('thumbnail')}}"
+                           required
+                    >
+                    @error('thumbnail')
+                    <p class="text-red-500 text-xs mt-2">{{ $message}}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-sx text-gray-700"
                            for="excerpt"
                     >
                         Excerpt
@@ -53,7 +75,7 @@
                               name="excerpt"
                               id="excerpt"
                               required
-                    > {{old('excerpt')}}"</textarea>
+                    > {{old('excerpt')}}</textarea>
 
                     @error('excerpt')
                     <p class="text-red-500 text-xs mt-2">{{ $message}}</p>
@@ -70,7 +92,7 @@
                               name="body"
                               id="body"
                               required
-                    >{{old('body')}}"</textarea>
+                    >{{old('body')}}</textarea>
 
                     @error('body')
                     <p class="text-red-500 text-xs mt-2">{{ $message}}</p>
