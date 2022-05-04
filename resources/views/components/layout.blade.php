39ff605bbd0ee724e4e86e6d47c8d743fc2cb9a0
ChrysostomosKas <57672898+ChrysostomosKas@users.noreply.github.com>
2022-05-04 14:11:10 +0300

    Episode 66->Extend the Admin Layou

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,21 +23,28 @@
         <div class="mt-8 md:mt-0 flex items-center">
             {{--If you are in show this msg else show the link--}}
             @auth()
-                <span class="text-xs font-bold uppercase">Welcome, {{auth()->user()->name}}!</span>
+                <x-dropdown>
+                    <x-slot name="trigger">
+                        <button class="text-xs font-bold uppercase">Welcome, {{auth()->user()->name}}!</button>
+                    </x-slot>
 
-                <form method="POST" action="/logout" class="text-xs font-semi   bold text-blue-500 ml-6">
-                    @csrf
+                    <x-dropdown-item href="/admin/dashboard">Dashboard</x-dropdown-item>
+                    <x-dropdown-item href="/admin/posts/create" :active="request()->is('admin/posts/create')">New Post</x-dropdown-item>
+                    <x-dropdown-item href="#" x-data="{}" @click.prevent="document.querySelector('#logout-form').submit()">Log Out</x-dropdown-item>
 
-                    <button type="submit">
-                        Log Out
-                    </button>
-                </form>
+
+                    <form id="logout-form" method="POST" action="/logout" class="text-xs font-semi   bold text-blue-500 ml-6">
+                        @csrf
+                    </form>
+                </x-dropdown>
+
             @else
                 <a href="/register" class="text-xs font-bold uppercase">Register</a>
                 <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
             @endauth
 
-            <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+            <a href="#newsletter"
+               class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                 Subscribe for Updates
             </a>
         </div>
@@ -45,7 +52,8 @@
 
     {{ $slot }}
 
-    <footer id="newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+    <footer id="newsletter"
+            class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
         <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
         <h5 class="text-3xl">Stay in touch with the latest posts</h5>
         <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
